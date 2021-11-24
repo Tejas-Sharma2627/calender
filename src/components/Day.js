aa581970535538318204dc1ffec56eb76f897f3a
Tejas_Sharma <tejassharma_ee20a15_36@dtu.ac.in>
2021-11-24 21:35:50 +0530

    Added the data.js and reading data

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,25 +1,54 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import dayjs from "dayjs";
 import GlobalContext from "../context/GlobalContext";
+import data from "../data";
 function Day({ day, rowInd }) {
-  const{showEnlargeTab, setShowEnlargeTab}=useContext(GlobalContext);
-  const handleClick=(e)=>{
-    console.log(typeof day.format('DD-MM-YY'));
-  }
-  const handleEnlargeTab=()=>{
+  const { showEnlargeTab, setShowEnlargeTab } = useContext(GlobalContext);
+  const handleClick = (e) => {
+    console.log(day.format("DD-MM-YY"));
+  };
+  const handleEnlargeTab = () => {
     setShowEnlargeTab(true);
-  }
+    // console.log(data.values(day.format("DD-MM-YY")))
+  };
+
   return (
-    
-    <div className={`border border-gray-200 flex flex-col `} >
+    <div className={`border border-gray-200 flex flex-col `}>
       <header className={`flex flex-col items-center `}>
         {rowInd === 0 && (
           <p className="text-sm mt-1">{day.format("dddd").toUpperCase()}</p>
         )}
-        <p className={`text-sm p-1 my-1 text-center ${day.format('DD-MM-YY')===dayjs().format('DD-MM-YY')&&'bg-blue-900 text-white rounded '}`} onClick={handleClick}>{day.format("DD")}</p>
-        <p className={`text-sm my-1 text-center ${day.format('DD-MM-YY')===dayjs().format('DD-MM-YY')&&'bg-blue-900 text-white rounded '}`} onClick={handleEnlargeTab}>Entered</p>
-        <p className={`text-sm my-1 text-left ${day.format('DD-MM-YY')===dayjs().format('DD-MM-YY')&&'bg-blue-900 text-white rounded '}`} onClick={handleEnlargeTab}>Exit</p>
-
+        <p
+          className={`text-sm p-1 my-1 text-center ${
+            day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") &&
+            "bg-blue-900 text-white rounded "
+          }`}
+          onClick={handleClick}
+        >
+          {day.format("DD")}
+        </p>
+        {data.hasOwnProperty(day.format("DD-MM-YY")) && (
+          <div>
+            <p
+              className={`text-sm my-1 text-center ${
+                day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") &&
+                "bg-blue-900 text-white rounded "
+              }`}
+              onClick={handleEnlargeTab}
+            >
+              Entered
+            </p>
+            <p
+              className={`text-sm  my-1 text-left ${
+                day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") &&
+                "bg-blue-900 text-white rounded "
+              }`}
+              onClick={handleEnlargeTab}
+            >
+              Exit
+            </p>
+          </div>
+        )}
       </header>
     </div>
   );
